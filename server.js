a6f20ad534afff05ac2a6dc98c2bb9910d105eae
Samson Lasode <samflicks@gmail.com>
2021-07-24 13:11:04 +0100

    updated server.js

--- server.js
+++ server.js
@@ -4,11 +4,8 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
-try {
-  await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-} catch (error) {
-  handleError(error);
-}
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
