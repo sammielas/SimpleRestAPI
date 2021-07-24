46e369661a879140a4e99b86fbe6201c7eb5cba7
Samson Lasode <samflicks@gmail.com>
2021-07-24 10:34:51 +0100

    set port number

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-require('dotenv').config()
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Database"));
 
-app.use(express.json())
+app.use(express.json());
 
-const subscribersRouter = require('./routes/subscribers')
-app.use('/subscribers',subscribersRouter)
+const subscribersRouter = require("./routes/subscribers");
+app.use("/subscribers", subscribersRouter);
 
-app.listen(3000, () => console.log("server running"));
+app.listen(PORT, () => console.log("server running"));
